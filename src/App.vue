f1aa8843fe0130d3c79aa4ec93fdcc8cc0ff1fce
thasup <thanachonfirst@hotmail.com>
2022-04-21 01:18:16 +0700

    Add: flash message with global storage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
-  <nav>
+<div id="app">
+  <div>
+    <div id="flashMessage" v-if="GStore.flashMessage" >{{ GStore.flashMessage }}</div>
+  </div>
+  <div id="nav">
     <router-link :to="{ name: 'EventList' }">Events</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
-  </nav>
+  </div>
   <router-view/>
+  </div>
 </template>
 
+<script>
+export default {
+  inject: ["GStore"]
+}
+</script>
+
 <style>
+@keyframes yellowfade {
+  from {
+    background: #42b983;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +40,16 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
 }
 
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -7,8 +7,15 @@
 
 export default {
     props: ["event"],
+    inject: ["GStore"],
     methods: {
         register() {
+            this.GStore.flashMessage = "You are successfully registered for " + this.event.title
+
+            setTimeout(() => {
+                this.GStore.flashMessage = ""
+            }, 3000)
+
             this.$router.push({
                 name: "EventDetails",
             })
